6879ca1d078504909fd113a61f1d1548a4d05d55
shry44 <shry.tkd@gmail.com>
2020-06-06 11:45:48 +0900

    activehashアソシエーション追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,6 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
   belongs_to              :seller         ,class_name: "User"
   has_many                :images         ,dependent: :destroy
   belongs_to              :category
@@ -13,6 +15,7 @@ class Item < ApplicationRecord
   belongs_to_active_hash  :shipping_period
   belongs_to_active_hash  :shipping_way
   belongs_to_active_hash  :size
+  belongs_to_active_hash  :status
 
   validates :name, :price, :category, :seller, :condition, :size, :shipping_fee, :prefecture, :shipping_period, :shipping_way, :status, presence: true
   validates :price, numericality: { only_integer: true, greater_than: 0, less_than: 9999999 }
