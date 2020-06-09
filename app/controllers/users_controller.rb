35c34f8d908ae9533c71e79982f4d39f69df118a
eclair6632 <masatoshi.moteki@gmail.com>
2020-06-09 19:45:36 +0900

    Temporarily stored_2
    一旦保存二回目

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,2 @@
 class UsersController < ApplicationController
-  def show
-  end
-
-  def cards
-  end
-
-  def deliver_address
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,20 @@
 Rails.application.routes.draw do
   devise_for :users
+  # マイページトップ
   get "users/show"
+  # マイページ-プロフィール
+  get "users/profile"
+  # マイページ-やることリスト
+  get "users/todo"
+  # マイページ-いいね！一覧
+  get "users/like"
+  # マイページ-クレジットカード一覧
   get "users/cards"
-  get "users/deliver_address"
+  # マイページ-クレジットカード登録
+  get "users/create"
+  # マイページ-住所変更
+  get "users/deliver-address"
+  # マイページ-ログアウト
+  get "users/logout"
+  
 end
