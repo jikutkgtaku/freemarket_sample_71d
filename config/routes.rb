75c47e7b1d88391df30f3ee3fc4066f0054af8e3
eclair6632 <masatoshi.moteki@gmail.com>
2020-06-13 00:38:01 +0900

    Create-markup_revision0

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   root "items#index"
   resources :items
+  
+  # マイページトップ（仮）
+  get "users/show"
+  # マイページ-支払い方法（仮）
+  get "users/cards"
+  # マイページ-クレジットカード登録（仮）
+  get "users/card-create"
+  # マイページ-ログアウト（仮）
+  get "users/logout"
 end
