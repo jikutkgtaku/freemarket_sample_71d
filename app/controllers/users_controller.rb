72dbe65758bb2fecb185aece8b7ed4c25004cbc3
springgarden29 <nacchi0729summer@gmail.com>
2020-06-14 11:26:39 +0900

    show action privateの上へ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,11 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show]
 
+  def show
+  end
+
   private
   def set_user
     @user = User.find(params[:id])
   end
-
-  def show
-  end
-  
 end
