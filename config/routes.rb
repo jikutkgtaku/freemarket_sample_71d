63006e0344de69d7e6ac9172ef2d2bcffeb1a53e
springgarden29 <nacchi0729summer@gmail.com>
2020-06-14 15:40:28 +0900

    コントローラー、ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,21 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root "items#index"
-  resources :items
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+  }
 
-  resources :users, only: :show do
-    collection do
-      get "new_login"
-      get "new_session"
-      get "new_user"
-      get "new_address"
-      get "create_address"
+  root "items#index"
+  # resources :items
+
+  # resources :users, only: :show do
+  #   collection do
+  #     get "new_login"
+  #     get "new_session"
+  #     get "new_user"
+  #     get "new_address"
+  #     get "create_address"
       
-    end
-  end
+  #   end
+  # end
+  resources :users, only: :show
 end
 
