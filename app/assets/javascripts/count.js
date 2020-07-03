a77a29fa074ab482ca1af530531acf78e0cfb14c
jikutkgtaku <tkg.jikutakuji@gmail.com>
2020-07-03 22:25:52 +0900

    Update count.js

--- app/assets/javascripts/count.js
+++ app/assets/javascripts/count.js
@@ -2,9 +2,6 @@ $(function(){
 // 商品の説明の文字数カウント
   var count = $(".items--text__field").text().replace(/\n/g, "改行").length;
   var now_count = 1000 - count
-  if (count > 1000) {
-    $(".items--text").css("color", red);
-  }
   $(".items--text__count").text("残り" + now_count + "文字");
   $(".items--text__field").on("keyup", function() {
     var count = $(this).val().replace(/\n/g, "改行").length;
