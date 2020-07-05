88952e3b9645c0058db9a729b86c1fd3e0893aed
springgarden29 <nacchi0729summer@gmail.com>
2020-07-05 14:56:47 +0900

    販売手数料計算、販売利益表示

---
+++ app/assets/javascripts/price.js
@@ -0,0 +1,20 @@
+
+$(function(){
+  $('.cost--selling--number__price').on('input', function(){   //リアルタイムで表示したいのでinputを使う｡入力の度にイベントが発火するようになる｡
+    var data = $('.cost--selling--number__price').val(); // val()でフォームのvalueを取得(数値)
+    var profit = Math.round(data * 0.9)  // 手数料計算を行う｡dataにかけているのが0.9なのは単に引きたい手数料が10%のため｡
+    var fee = (data - profit) // 入力した数値から計算結果(profit)を引く｡それが手数料となる｡
+    $('.gain-commission').html(fee) //  手数料の表示｡html()は追加ではなく､上書き｡入力値が変わる度に表示も変わるようにする｡
+    $('.gain-commission').prepend('¥') // 手数料の前に¥マークを付けたいので
+    $('.gain-profit ').html(profit)
+    $('.gain-profit ').prepend('¥')
+    $('#price').val(profit) // 計算結果を格納用フォームに追加｡もし､入力値を追加したいのなら､今回はdataを引数に持たせる｡
+    if(profit == '') {   // もし､計算結果が''なら表示も消す｡
+    $('.gain-profit ').html('');
+    $('.gain-commission').html('');
+    }
+  })
+})
+
+
+
