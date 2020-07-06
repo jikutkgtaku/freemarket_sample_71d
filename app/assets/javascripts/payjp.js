092bf68d40897fb6f1acf2e939685dc37c8334df
jikutkgtaku <tkg.jikutakuji@gmail.com>
2020-07-06 20:20:43 +0900

    [WIP]payjp.js

--- app/assets/javascripts/payjp.js
+++ app/assets/javascripts/payjp.js
@@ -1,3 +1,31 @@
 window.addEventListener('DOMContentLoaded', function(){
-  Payjp,setPublicKey("pk_test_")
+  Payjp.setPublicKey("pk_test_1ec0ec04db6b232048c16e77");
+
+  let submit = document.getElementById("payment_card_submit-button");
+  submit.addEventListener('click', function(e){
+    e.preventDefault();
+
+    let card = {
+      number: document.getElementById("payment_card_no").value,
+      cvc: document.getElementById("payment_card_cvc").value,
+      exp_month: document.getElementById("payment_card_month").value,
+      exp_year: document.getElementById("payment_card_year").value
+    };
+
+    Payjp.createToken(card, function(status, response) {
+      if (status === 200) {
+        $(".number").removeAttr("name");
+        $(".cvc").removeAttr("name");
+        $(".exp_month").removeAttr("name");
+        $(".exp_year").removeAttr("name");
+        $("#charge-form").append(
+          $('<input type="hidden" name="payjp_token>').val(response.id)
+        );
+        document.inputForm.submit();
+        alert("登録が完了しました"); //正常処理完了確認用。createビューがあればつけなくてもOKかな
+      } else {
+        alert("カード情報が正しくありません。"); //エラー確認用
+      }
+    })
+  })
 })
