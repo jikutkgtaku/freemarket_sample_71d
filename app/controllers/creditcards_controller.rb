214d45498536dcffae803cb96c335f09c74b25a4
jikutkgtaku <tkg.jikutakuji@gmail.com>
2020-07-11 09:57:17 +0900

    Update creditcards_controller.rb

--- app/controllers/creditcards_controller.rb
+++ app/controllers/creditcards_controller.rb
@@ -10,12 +10,12 @@ class CreditcardsController < ApplicationController
     #credential.ymlに登録したpayjpのapiキーの取得
     Payjp.api_key = Rails.application.credentials.dig(:payjp, :PAYJP_SECRET_KEY)
     #falshメッセージを導入しているため、後ほど実装
-    if params["payjp_token"].blank?
+    if params['payjp_token'].blank?
       redirect_to action: "new", alert: "クレジットカードを登録できませんでした。"
     else
       customer = Payjp::Customer.create(
         email: current_user.email,
-        card: params["payjp_token"],
+        card: params['payjp_token'],
         metadate: {user_id: current_user.id}
       )
       @card = Creditcard.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
