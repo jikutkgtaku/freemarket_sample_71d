12ff8ea72272691f6ab0466abf9f287e66cb142c
springgarden29 <nacchi0729summer@gmail.com>
2020-07-18 16:26:08 +0900

    テストコード

---
+++ spec/factories/items.rb
@@ -0,0 +1,16 @@
+FactoryBot.define do
+
+  factory :item do
+    name                      {"sasaki"}
+    introduction              {"コメント"}
+    price                     {"2000"}
+    category_id               {"1"}
+    condition_id              {"1"}   
+    shipping_fee_id           {"1"}    
+    prefecture_id             {"1"} 
+    shipping_period_id        {"1"}
+    shipping_way_id           {"1"}
+    status_id                 {"1"}
+  end
+
+end
