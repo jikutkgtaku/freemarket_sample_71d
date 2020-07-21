ab5782465b769328ddaf7ca122e80c2fa9a92c0d
jikutkgtaku <tkg.jikutakuji@gmail.com>
2020-07-21 23:04:42 +0900

    Update creditcard_spec.rb

--- spec/models/creditcard_spec.rb
+++ spec/models/creditcard_spec.rb
@@ -1,24 +1,25 @@
 require 'rails_helper'
 
-RSpec.describe Creditcard, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Creditcard do
+  describe '#create' do
+
+    it "customer_idがない場合は登録できないこと" do
+      creditcard = build(:creditcard, user_id: "")
+      creditcard.valid?
+      expect(creditcard.errors[:user_id]).to include()
+    end
+
+    it "customer_idがない場合は登録できないこと" do
+      creditcard = build(:creditcard, customer_id: "")
+      creditcard.valid?
+      expect(creditcard.errors[:customer_id]).to include()
+    end
+
+    it "payjp_idがない場合は登録できないこと" do
+      creditcard = build(:creditcard, payjp_id: "")
+      creditcard.valid?
+      expect(creditcard.errors[:payjp_id]).to include()
+    end
+
+  end
 end
-
-# describe Creditcard do
-#   describe '#create' do
-
-#     it "customer_idがない場合は登録できないこと" do
-#       creditcard = build(:creditcard, customer_id: "")
-#       creditcard.valid?
-#       expect(creditcard.errors[:customer_id]).to include("を入力してください")
-#     end
-
-#     it "payjp_idがない場合は登録できないこと" do
-#       creditcard = build(:creditcard, payjp_id: "")
-#       creditcard.valid?
-#       expect(creditcard.errors[:payjp_id]).to include("を入力してください")
-#     end
-
-
-#   end
-# end
