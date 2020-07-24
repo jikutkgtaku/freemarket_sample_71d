884072190bfca9d76916afb417b2f2f493d148c4
jikutkgtaku <tkg.jikutakuji@gmail.com>
2020-07-24 16:11:22 +0900

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@ CSV.foreach('db/seeds/brand.csv', headers: true) do |row|
     name: row['name']
   )
 end
+
+require './db/seeds/category_size.rb'
+
+require './db/seeds/category.rb'
+
+require './db/seeds/size.rb'
