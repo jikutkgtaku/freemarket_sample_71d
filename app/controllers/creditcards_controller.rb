7a1fd87826729296c7784454a1f004336b7e1973
jikutkgtaku <tkg.jikutakuji@gmail.com>
2020-07-26 10:26:58 +0900

    flashアラート、商品出品関係微修正

--- app/controllers/creditcards_controller.rb
+++ app/controllers/creditcards_controller.rb
@@ -85,41 +85,44 @@ class CreditcardsController < ApplicationController
     @images = @item.images.all
     
     @address = Address.find(current_user.id)
-
-    if @item.buyer.present?
-      redirect_to item_path(@item.id), alert: "売り切れています"
+    if @item.seller == User.find(current_user.id)
+      redirect_to item_path(@item.id), alert: "あなたが出品した商品です"
     else
-      if user_signed_in?
-        @user = current_user
-
-        if @user.creditcard.present?
-          Payjp.api_key = Rails.application.credentials.dig(:payjp, :PAYJP_SECRET_KEY)
-          @card = Creditcard.find_by(user_id: current_user.id)
-          customer = Payjp::Customer.retrieve(@card.customer_id)
-          @customer_card = customer.cards.retrieve(@card.payjp_id)
-
-          @card_brand = @customer_card.brand
-          case @card_brand
-          when "Visa"
-            @card_src = "visa.png"
-          when "JCB"
-            @card_src = "jcb.png"
-          when "MasterCard"
-            @card_src = "master.png"
-          when "American Express"
-            @card_src = "amex.png"
-          when "Diners Club"
-            @card_src = "diners.png"
-          when "Discover"
-            @card_src = "discover.png"
-          end
-
-          @exp_month = @customer_card.exp_month.to_s
-          @exp_year = @customer_card.exp_year.to_s.slice(2,3)
-        else
-        end
+      if @item.buyer.present?
+        redirect_to item_path(@item.id), alert: "売り切れています"
       else
-        redirect_to user_session_path, alert: "ログインしてください"
+        if user_signed_in?
+          @user = current_user
+
+          if @user.creditcard.present?
+            Payjp.api_key = Rails.application.credentials.dig(:payjp, :PAYJP_SECRET_KEY)
+            @card = Creditcard.find_by(user_id: current_user.id)
+            customer = Payjp::Customer.retrieve(@card.customer_id)
+            @customer_card = customer.cards.retrieve(@card.payjp_id)
+
+            @card_brand = @customer_card.brand
+            case @card_brand
+            when "Visa"
+              @card_src = "visa.png"
+            when "JCB"
+              @card_src = "jcb.png"
+            when "MasterCard"
+              @card_src = "master.png"
+            when "American Express"
+              @card_src = "amex.png"
+            when "Diners Club"
+              @card_src = "diners.png"
+            when "Discover"
+              @card_src = "discover.png"
+            end
+
+            @exp_month = @customer_card.exp_month.to_s
+            @exp_year = @customer_card.exp_year.to_s.slice(2,3)
+          else
+          end
+        else
+          redirect_to user_session_path, alert: "ログインしてください"
+        end
       end
     end
   end
