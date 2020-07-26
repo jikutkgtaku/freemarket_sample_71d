cdfcfac679b9a1b54a328b98ea638b38c356facc
eclair6632 <masatoshi.moteki@gmail.com>
2020-07-26 22:06:05 +0900

    商品画面の編集（削除はできない。）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -33,6 +33,7 @@ class ItemsController < ApplicationController
   
   def update
     @item = Item.find(params[:id])
+    # binding.pry
     if @item.update(item_params)
       redirect_to root_path
     else
